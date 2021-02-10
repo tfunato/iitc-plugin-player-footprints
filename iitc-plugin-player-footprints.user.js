6992a8e28230c7051991583a3ac3bcd3a2bea293
tfunato <tfunato@gmail.com>
2021-02-10 23:56:57 +0900

    fixed indent

--- iitc-plugin-player-footprints.user.js
+++ iitc-plugin-player-footprints.user.js
@@ -7,8 +7,8 @@
 // @updateURL https://raw.githubusercontent.com/tfunato/iitc-plugin-player-footprints/main/iitc-plugin-player-footprints.user.js?inline=false
 // @downloadURL https://raw.githubusercontent.com/tfunato/iitc-plugin-player-footprints/main/iitc-plugin-player-footprints.user.js?inline=false
 // @description Show Player footprints on the map.
-// @include		https://intel.ingress.com/*
-// @match		https://intel.ingress.com/*
+// @include        https://intel.ingress.com/*
+// @match        https://intel.ingress.com/*
 // @grant          none
 // ==/UserScript==
 
@@ -51,13 +51,13 @@ function wrapper(plugin_info) {
     };
 
     thisPlugin.highlightFootprint = function(portal, type) {
-		const scale = map.getZoom();
-		const portalLatLng = L.latLng(portal._latlng.lat, portal._latlng.lng);
-		const lvlWeight = 30;
-		const lvlRadius = 2;
+        const scale = map.getZoom();
+        const portalLatLng = L.latLng(portal._latlng.lat, portal._latlng.lng);
+        const lvlWeight = 30;
+        const lvlRadius = 2;
         const color = footprintColor[type];
-		thisPlugin.playerFootprintsLayerGroup.addLayer(L.circleMarker(portalLatLng, { radius: lvlRadius, fill: false, color: color, weight: lvlWeight, interactive: false, clickable: false }));
-	}
+        thisPlugin.playerFootprintsLayerGroup.addLayer(L.circleMarker(portalLatLng, { radius: lvlRadius, fill: false, color: color, weight: lvlWeight, interactive: false, clickable: false }));
+    }
 
     thisPlugin.clearAllFootprints = function() {
         for (let layer in thisPlugin.playerFootprintsLayerGroup._layers) {
@@ -66,12 +66,12 @@ function wrapper(plugin_info) {
     }
 
     function getLatLngPoint(data) {
-		const result = {
-			lat: typeof data.lat == 'function' ? data.lat() : data.lat,
-			lng: typeof data.lng == 'function' ? data.lng() : data.lng
-		};
-		return result;
-	}
+        const result = {
+            lat: typeof data.lat == 'function' ? data.lat() : data.lat,
+            lng: typeof data.lng == 'function' ? data.lng() : data.lng
+        };
+        return result;
+    }
 
     // ass calculating portal marker visibility can take some time when there's lots of portals shown, we'll do it on
     // a short timer. this way it doesn't get repeated so much
@@ -93,7 +93,7 @@ function wrapper(plugin_info) {
    }
 
     setup.info = plugin_info; //add the script info data to the function as a property
-	// if IITC has already booted, immediately run the 'setup' function
+    // if IITC has already booted, immediately run the 'setup' function
     if (window.iitcLoaded) {
         setup();
     } else {
@@ -107,22 +107,22 @@ function wrapper(plugin_info) {
 // PLUGIN END //////////////////////////////////////////////////////////
 
 (function () {
-	const plugin_info = {};
-	if (typeof GM_info !== 'undefined' && GM_info && GM_info.script) {
-		plugin_info.script = {
-			version: GM_info.script.version,
-			name: GM_info.script.name,
-			description: GM_info.script.description
-		};
-	}
-	// Greasemonkey. It will be quite hard to debug
-	if (typeof unsafeWindow != 'undefined' || typeof GM_info == 'undefined' || GM_info.scriptHandler != 'Tampermonkey') {
-	// inject code into site context
-		const script = document.createElement('script');
-		script.appendChild(document.createTextNode('(' + wrapper + ')(' + JSON.stringify(plugin_info) + ');'));
-		(document.body || document.head || document.documentElement).appendChild(script);
-	} else {
-		// Tampermonkey, run code directly
-		wrapper(plugin_info);
-	}
+    const plugin_info = {};
+    if (typeof GM_info !== 'undefined' && GM_info && GM_info.script) {
+        plugin_info.script = {
+            version: GM_info.script.version,
+            name: GM_info.script.name,
+            description: GM_info.script.description
+        };
+    }
+    // Greasemonkey. It will be quite hard to debug
+    if (typeof unsafeWindow != 'undefined' || typeof GM_info == 'undefined' || GM_info.scriptHandler != 'Tampermonkey') {
+    // inject code into site context
+        const script = document.createElement('script');
+        script.appendChild(document.createTextNode('(' + wrapper + ')(' + JSON.stringify(plugin_info) + ');'));
+        (document.body || document.head || document.documentElement).appendChild(script);
+    } else {
+        // Tampermonkey, run code directly
+        wrapper(plugin_info);
+    }
 })();
